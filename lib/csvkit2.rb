3a60d033f9d5d863fb1b1404db7ff07716dd488a
Seamus Abshere <seamus@abshere.net>
2013-11-04 09:09:00 -0600

    bundle gem csvkit2 -b -t

---
+++ lib/csvkit2.rb
@@ -0,0 +1,5 @@
+require "csvkit2/version"
+
+module Csvkit2
+  # Your code goes here...
+end
---
+++ spec/csvkit2_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Csvkit2 do
+  it 'should have a version number' do
+    Csvkit2::VERSION.should_not be_nil
+  end
+
+  it 'should do something useful' do
+    false.should be_true
+  end
+end

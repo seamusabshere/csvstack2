eb9d3be6c76cd38677ee60419154ae93633757b0
Seamus Abshere <seamus@abshere.net>
2013-11-04 10:04:02 -0600

    csvstack2 reorders columns as necessary

--- spec/csvkit2_spec.rb
+++ spec/csvkit2_spec.rb
@@ -1,11 +1,37 @@
 require 'spec_helper'
 
 describe Csvkit2 do
-  it 'should have a version number' do
-    Csvkit2::VERSION.should_not be_nil
-  end
+  include SpecHelper
+  describe 'csvstack2' do
+    it "stacks 2 csv files" do
+      out = run 'csvstack2', 'a_b.csv', 'a_b.csv'
+      assert_csv(out, %{
+        a,b
+        1,2
+        1,2
+      })
+      File.unlink out
+    end
 
-  it 'should do something useful' do
-    false.should be_true
+    it "stacks 3 csv files" do
+      out = run 'csvstack2', 'a_b.csv', 'a_b.csv', 'a_b.csv'
+      assert_csv(out, %{
+        a,b
+        1,2
+        1,2
+        1,2
+      })
+      File.unlink out
+    end
+
+    it "stacks csv files with same headers in different order" do
+      out = run 'csvstack2', 'a_b.csv', 'b_a.csv'
+      assert_csv(out, %{
+        a,b
+        1,2
+        1,2
+      })
+      File.unlink out
+    end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,27 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'csvkit2'
+
+require 'csv'
+require 'tmpdir'
+require 'shellwords'
+
+module SpecHelper
+  def run(*args)
+    out_path = File.join(Dir.tmpdir, "#{args.join('_').gsub(/\W/, '_')}-#{Time.now.to_f}.csv")
+    bin_path = File.expand_path("../../bin/#{args.shift}", __FILE__)
+    in_paths = args.map { |path| File.expand_path("../support/#{path}", __FILE__) }
+    cmd = "bash -c 'bundle exec #{bin_path} #{in_paths.map { |path| Shellwords.escape(path) }.join(' ')}' > #{out_path}"
+    stdout = `#{cmd}`
+    unless $?.success?
+      $stderr.puts "boom: #{cmd}"
+      $stderr.puts stdout
+    end
+    out_path
+  end
+
+  def assert_csv(got_csv_path, expected_csv)
+    got = CSV.read(got_csv_path, headers: true).to_s
+    expected = CSV.parse(expected_csv.gsub(/^ */, '').strip, headers: true).to_s
+    got.should == expected
+  end
+end
